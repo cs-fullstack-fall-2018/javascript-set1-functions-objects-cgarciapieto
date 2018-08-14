13fb55e8f9b3ae5540ce5ebe57885e577c7dd623
Carlos Garcia <cgarciapieto@gmail.com>
2018-08-13 23:06:41 -0500

    javascript-set1-functions-objects-cgarciapieto

--- homework.js
+++ homework.js
@@ -21,3 +21,10 @@ personObjects4.firstname = ("Lemmy");
 personObjects4.lastname = ("Kilmister");
 personObjects4.age = (57);
 personObjects4.zipcode = (21120);
+
+
+
+"<tr><td align="center"> Jimmy </td><td align="center"> Page </td><td align="center"> 62 </td><td align="center"> 10820 </td></tr>"
+"<tr><td align="center"> + personObjects2.firstname + "</td><td align="center"> + personObjects2.lastname+ "</td><td align="center"> + personObjects2.age+ "</td><td align="center"> + personObjects2.zipcode+ "</td></tr>"
+"<tr><td align="center"> + personObjects3.firstname + "</td><td align="center"> + personObjects3.lastname+ "</td><td align="center"> + personObjects3.age+ "</td><td align="center"> + personObjects3.zipcode+ "</td></tr>"
+"<tr><td align="center"> + personObjects4.firstname + "</td><td align="center"> + personObjects4.lastname+ "</td><td align="center"> + personObjects4.age+ "</td><td align="center"> + personObjects4.zipcode+ "</td></tr>"
