dbb7ae8846acde00ee6cfb914c741ed18d213219
Carlos Garcia <cgarciapieto@gmail.com>
2018-08-13 22:35:13 -0500

    javascript-set1-functions-objects-cgarciapieto

---
+++ homework.js
@@ -0,0 +1,23 @@
+var personObjects = {};
+personObjects.firstname = ("Jimmy");
+personObjects.lastname = ("Page");
+personObjects.age = (62);
+personObjects.zipcode = (10821);
+
+var personObjects2 = {};
+personObjects2.firstname = ("Rick Nielsen");
+personObjects2.lastname = ("Nielsen");
+personObjects2.age = (57);
+personObjects2.zipcode = (61016);
+
+var  personObjects3 = {};
+personObjects3.firstname = ("Joe");
+personObjects3.lastname = ("Walsh");
+personObjects3.age = (58);
+personObjects3.zipcode = (90001);
+
+var personObjects4 = {};
+personObjects4.firstname = ("Lemmy");
+personObjects4.lastname = ("Kilmister");
+personObjects4.age = (57);
+personObjects4.zipcode = (21120);
